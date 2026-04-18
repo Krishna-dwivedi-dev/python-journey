4a7d5c7e0bd9ce333a5260fccdeaf210aa3c31c9
krishna <krishnadwivedi927@gmail.com>
2026-04-18 21:45:51 +0530

    Day 1: Temperature converter, Multiplication table, Prime numbers

--- revision_notes.py
+++ revision_notes.py
@@ -1,1 +1,23 @@
-# Day 1 Revision 
+"""
+def function(a):
+    fahrenhite = (a * 9/5) + 32
+    kelvin = 273.15 + a
+    return(fahrenhite,kelvin)
+a = float(input("what is the temp in celcius"))
+result = function(a)
+print(result)
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(i, "X",j,"=",i*j)
+        """
+def is_prime(n):
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+for j in range(2,51):
+ if is_prime(j):
+    print(j)
+
+    
